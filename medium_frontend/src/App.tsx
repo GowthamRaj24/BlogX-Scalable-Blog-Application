f2b1f8843297358f84aacd3b5c880618b629c933
Gowtham Raj <gmanda@gitam.in>
2024-12-10 23:07:15 +0530

    added Routing

--- medium_frontend/src/App.tsx
+++ medium_frontend/src/App.tsx
@@ -1,7 +1,19 @@
+import {BrowserRouter , Routes , Route} from "react-router-dom";
+import Signup from "./pages/Signup";
+import Signin from "./pages/Signin";
+import Blog from "./pages/Blog"
+
 function App() {
 
   return (
     <>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signup" element={<Signin/>}/>
+        <Route path="/signin" element={<Signup/>} />
+        <Route path="/blog" element={<Blog/>} />
+      </Routes>
+    </BrowserRouter>
 
     </>
   )
