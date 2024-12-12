5f0dc471192933dea4865417110a18cf0f2f359a
Gowtham Raj <gmanda@gitam.in>
2024-12-12 20:09:02 +0530

    bug

--- medium_frontend/src/components/AuthInputs/AuthInputs.tsx
+++ medium_frontend/src/components/AuthInputs/AuthInputs.tsx
@@ -1,13 +1,21 @@
 import "./AuthInputs.css";
 
 const AuthInputs = (props:any) => {
+    const handleOnChange = (e:any) => {
+        const { name, value } = e.target;
+        props.setInput({
+            ...props.inputValues,
+            [name]: value
+        });
+    }
+
     return(<>
     <div className="outer-auth-inputs">
         <div className="auth-inputs-title">
             {props.title}
         </div>
         <div className="auth-inputs">
-            <input type={props.type} className="auth-input" placeholder={props.placeholder} />
+            <input type={props.type} className="auth-input" placeholder={props.placeholder} onChange={handleOnChange}/>
         </div>
     </div>
     </>)
--- medium_frontend/src/pages/Signup.tsx
+++ medium_frontend/src/pages/Signup.tsx
@@ -1,13 +1,32 @@
 import Quote from "../components/Quote/Quote";
 import "./Signup.css";
 import Auth from "../components/Auth/Auth";
+import { useEffect, useState } from "react";
 
 const Signup = () => {
+    const initialInputs = {
+        Username: "",
+        Email: "",
+        Password: "",
+        Reenter_Password: ""
+    };
+
+    const [input, setInput] = useState(initialInputs);
+
+    useEffect(()=>{
+        console.log(input)
+    }, [input])
+
     return (
         <>
             <div className="signup-main">
-                <div className="singup-main-outer">
-                    <Auth inputs={["Username" , "Email" , "Password", "Reenter Password"]} page={"signup"} />
+                <div className="signup-main-outer">
+                    <Auth 
+                        inputs={["Username", "Email", "Password", "Reenter_Password"]} 
+                        page={"signup"} 
+                        setInput={setInput}
+                        inputValues={input}
+                    />
                 </div>
                 <div className="small-invisible">
                     <Quote />
